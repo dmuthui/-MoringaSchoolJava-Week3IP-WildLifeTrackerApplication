a5008dc58eb0feb6ca7fbd3d6d584ba295cde86e
dmuthui <vyda2002us@gmail.com>
2023-06-14 03:30:13 +0300

    Adds the changes to project styling with app.css,app.js,home.hbs and layout.hbs

---
+++ src/main/resources/public/app.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+// Animations init
+new WOW().init();
+
+ var typed = new Typed("#type", {
+    strings: [
+      "<span style='color:purple;'>Wildlife Tracker</span>",
+      "<span style='color:purple;'>An application that allows Rangers to track wildlife sightings in the area.</span>"
+    ],
+    backSpeed: 70,
+    typeSpeed: 80,
+    smartBackspace: true,
+    loop: true
+  });
+
+   var typed =new Typed(`#msg`,{
+        strings:[" Your Animal has been saved ! "],
+        backSpeed:70,
+        typeSpeed:80,
+        smartBackspace:true,
+      })
+})
+
+
+function secondClick(){
+timer:4000
+Swal.fire(
+  'View Animals?',
+)
+}
+
+function firstClick(){
+timer:4000
+Swal.fire(
+  'Add endangered?',
+
+)
+}
+function thirdClick(){
+timer:4000
+Swal.fire(
+  'Record/Report Sighting?',
+
+)
+}
+
+
+
+
+
+
+
+// Animations init
+new WOW().init();
