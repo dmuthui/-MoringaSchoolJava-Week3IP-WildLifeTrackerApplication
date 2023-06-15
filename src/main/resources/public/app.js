e5a5a64a123f8d3548c16ec6ddf51968c1996428
dmuthui <vyda2002us@gmail.com>
2023-06-15 23:26:56 +0300

    Adds the modification of:app.js,endangeredAnimalForm.hbs,home.hbs and layout.hbs

--- src/main/resources/public/app.js
+++ src/main/resources/public/app.js
@@ -1,48 +1,17 @@
-$(document).ready(function(){
-// Animations init
-new WOW().init();
+$(document).ready(function() {
+  // Initialize WOW.js for animations
+  new WOW().init();
 
- var typed = new Typed("#type", {
+  // Typed.js for typing effect
+  var typed = new Typed("#type", {
     strings: [
       "<span style='color:black;'>Wildlife Tracker</span>",
-      "<span style='color:black;'>An application that allows Rangers to track wildlife sightings in the area on every occurence and record.</span>"
+      "<span style='color:black;'>An application that allows Rangers to track wildlife sightings in the area on every occurrence and record.</span>"
     ],
     backSpeed: 70,
     typeSpeed: 80,
     smartBackspace: true,
     loop: true
   });
+  });
 
-   var typed =new Typed(`#msg`,{
-        strings:[" Your Animal has been saved ! "],
-        backSpeed:70,
-        typeSpeed:80,
-        smartBackspace:true,
-      })
-})
-
-
-function secondClick(){
-timer:4000
-Swal.fire(
-  'Add Animal?',
-)
-}
-
-function firstClick(){
-timer:4000
-Swal.fire(
-  'Add endangered?',
-
-)
-}
-function thirdClick(){
-timer:4000
-Swal.fire(
-  'Record/Report Sighting?',
-
-)
-}
-
-// Animations init
-new WOW().init();
