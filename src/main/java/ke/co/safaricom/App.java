d1eae8fb4a0994c9d29f3e2c90b6570f3b62681b
dmuthui <vyda2002us@gmail.com>
2023-06-16 20:22:05 +0300

    Adds changes to App.java,aboutAppPage,addAnimalForm,endangeredAnimalForm,home,sightingsForm,app.css,app.js,

--- src/main/java/ke/co/safaricom/App.java
+++ src/main/java/ke/co/safaricom/App.java
@@ -1,13 +1,15 @@
 package ke.co.safaricom;
 
+import ke.co.safaricom.dao.RegularAnimalDao;
+import ke.co.safaricom.model.RegularAnimal;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
@@ -25,6 +27,34 @@ public class App {
         //THE ROUTE TO SERVE ADD ANIMAL AFTER CLICKING ADD ANIMAL BUTTON
         get("/add-animal", (request, response) -> {
             return new ModelAndView(new HashMap<>(), "addAnimalForm.hbs");
+        }, engine);
+
+        //ROUTE TO SERVE POSTING OF ADD REGULAR ANIMAL TO THE DATABASE
+        post("/add-animal", (request, response) -> {
+            // Initializing the regular animal
+            Integer regular_animal_id = null;
+            String animalName = request.queryParams("animalName");
+            String health = request.queryParams("health");
+            String age = request.queryParams("age");
+            Boolean deleted = false;
+            RegularAnimal additionalRegularAnimal = new RegularAnimal(regular_animal_id, animalName, health, age, deleted);
+            RegularAnimalDao.addRegularAnimal(additionalRegularAnimal);
+            response.redirect("/animal-list");
+            return null;
+        });
+       // THE ROUTE TO HANDLE GETTING ALL REGULAR ANIMALS FROM DATABASE ON THE VIEW REGULAR ANIMALS LIST
+        get("/animal-list", (req, res) -> {
+            Map<String, List<RegularAnimal>> regularAnimalList = new HashMap<>();
+            regularAnimalList.put("regularAnimals", RegularAnimalDao.getAllRegularAnimals());
+            return new ModelAndView(regularAnimalList, "allAnimals.hbs");
+        }, engine);
+
+        //DELETING A REGULAR ANIMAL FROM THE ANIMALS LIST PAGE
+        get("/delete-regularAnimal/:animalName", (req, res) -> {
+            String animalName = req.params(":animalName");
+            RegularAnimalDao.deleteRegularAnimal(animalName);
+            res.redirect("/animal-list");
+            return null;
         }, engine);
 
         //THE ROUTE TO SERVE SIGHTINGS AFTER CLICKING ADD REPORT/RECORD SIGHTINGS BUTTON
--- src/main/resources/public/app.js
+++ src/main/resources/public/app.js
@@ -15,3 +15,24 @@ $(document).ready(function() {
   });
   });
 
+//FUNCTION THAT GIVES AN ALERT IF YOU WANT TO DELETE A REGULAR ANIMAL
+//AN ALERT THAT POPS WHEN YOU WANT TO DELETE A HERO FROM THE HEROES LIST
+ function confirmDeletion(animalName) {
+     var result = confirm("Are you sure you want to delete the regular animal " + animalName + "?");
+     if (!result) {
+         return false; // Cancel button was clicked, return false to prevent deletion
+     }
+     // Perform the deletion action
+     window.location.href = "/delete-regularAnimal/" + animalName;
+ }
+
+ //VALIDATES ADDANIMALFORM DATA ENTRY:SENDS ALERT WHEN YOU CLICK BLANK FORM FOR SUBMISSION
+ function validateForm() {
+     var animalName = document.getElementById("animalName").value.toUpperCase().trim(); // Trim whitespace
+
+     if (animalName === "") {
+         alert("Please fill in all the fields before submitting the form.");
+         return false; // Prevent form submission
+     }
+
+ }
