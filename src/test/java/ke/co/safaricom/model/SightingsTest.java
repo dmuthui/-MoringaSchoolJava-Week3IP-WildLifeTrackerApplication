b0a520a33da3bd88ea71f8c3d1c33a17fcd857ac
dmuthui <vyda2002us@gmail.com>
2023-06-19 04:39:29 +0300

    Adds Rangers, RegularAnimal and Sighting Test files after confirming they are working as expected

--- src/test/java/ke/co/safaricom/model/SightingsTest.java
+++ src/test/java/ke/co/safaricom/model/SightingsTest.java
@@ -1,32 +1,51 @@
 package ke.co.safaricom.model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SightingsTest {
-
+    @DisplayName("Test to check whether the sighting_id passes the test")
     @Test
     void getSighting_id() {
+        Sightings mySighting = new Sightings(1, "Endangered", "White Rhino", "Zone C", "Emerald", false);
+        Integer expected = 1;
+        assertEquals(1, mySighting.getSighting_id());
     }
-
+    @DisplayName("Test to check whether the animal category passes the test")
     @Test
     void getAnimal_category() {
+        Sightings mySighting = new Sightings(1, "Endangered", "White Rhino", "Zone C", "Emerald", false);
+        String expected = "Endangered";
+        assertEquals("Endangered", mySighting.getAnimal_category());
     }
-
+    @DisplayName("Test to check whether the animal name passes the test")
     @Test
     void getAnimal_name() {
+        Sightings mySighting = new Sightings(1, "Endangered", "White Rhino", "Zone C", "Emerald", false);
+        String expected = "White Rhino";
+        assertEquals("White Rhino", mySighting.getAnimal_name());
     }
-
+    @DisplayName("Test to check whether the location passes the test")
     @Test
     void getLocation() {
+        Sightings mySighting = new Sightings(1, "Endangered", "White Rhino", "Zone C", "Emerald", false);
+        String expected = "Zone C";
+        assertEquals("Zone C", mySighting.getLocation());
     }
-
+    @DisplayName("Test to check whether the rangers name passes the test")
     @Test
     void getRangers_name() {
+        Sightings mySighting = new Sightings(1, "Endangered", "White Rhino", "Zone C", "Emerald", false);
+        String expected = "Emerald";
+        assertEquals("Emerald", mySighting.getRangers_name());
     }
-
+    @DisplayName("Test to check whether when deleted is false passes the test")
     @Test
     void getDeleted() {
+        Sightings mySighting = new Sightings(1, "Endangered", "White Rhino", "Zone C", "Emerald", false);
+        Boolean expected = false;
+        assertEquals(false, mySighting.getDeleted());
     }
 }
